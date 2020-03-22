348ddb6961f2818d13266ac54c3983745428c3dc
wendelleloi <wendelleloi2@gmail.com>
2020-03-22 18:25:11 -0300

    Aula 4 - 2

---
+++ wp_como_CMS/HTML_PARA_WORDPRESS/footer.php
@@ -0,0 +1,16 @@
+    <footer>
+			<nav class="nav-footer">
+				<ul>
+					<li class="current_page_item"><a href="index.html">Menu</a></li>
+					<li><a href="sobre.html">Sobre</a></li>
+					<li><a href="contato.html">Contato</a></li>
+				</ul>
+			</nav>
+
+			<p>Rest © 2015. Alguns direitos reservados.</p>
+		</footer>
+    <!-- Footer Wordpres -->
+    <?php wp_footer(); ?>
+    <!-- Footer Wordpres -->
+	</body>
+</html>
